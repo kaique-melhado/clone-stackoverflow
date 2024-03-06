1a2dbe726efc362523239558389c954493fcf592
Lucas Melhado <lucasmelhado23@gmail.com>
2024-03-06 12:46:28 -0300

    criando cadastro de usuario

---
+++ CloneStackOverflow/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using CloneStackOverflow.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloneStackOverflow.Controllers
+{
+    public class UserController : Controller
+    {
+        public UserController()
+        {
+                
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User userModel)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ CloneStackOverflow/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace CloneStackOverflow.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; } 
+
+        public string Nome { get; set; }    
+
+        public string Email { get; set;}
+
+        public string Senha { get; set;}
+
+        public DateTime DataCadastro { get; set;} 
+    }
+}
